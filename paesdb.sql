a61f8dcb350f021477004c4b54709b94fbae780e
GabrielAlbSilv <gabrielalbuquerquemoura@gmail.com>
2022-03-15 19:57:31 -0300

    Nova COMMIT X2

--- paesdb.sql
+++ paesdb.sql
@@ -16,8 +16,13 @@ CREATE TABLE `Paes` (
 INSERT INTO `Paes` (`id`, `nome`, `sabor`, `preco`, `padaria`)
 VALUES (1, 'Pão Francês', 'Salgado', 'R$ 00,30', 'Sr Euclides'),
 (2, 'Pão de Açúcar', 'Doce', 'R$ 1,00', 'Padaria Estrada Nova'),
-(3, 'AAAAAAA', 'Salgado', 'R$ 00,30', 'Sr Euclides')
-;
+(3, 'Pão de Queijo Grande', 'Salgado', 'R$ 2,50', 'Império do Pão de Queijo'),
+(4, 'Baguete', 'Salgado', 'R$ 2,98', 'Padaria Estrada Velha'),
+(5, 'Bisnaga', 'Doce', 'R$ 1,80', 'Sr Onófrio'),
+(6, 'Brioche', 'Doce', 'R$ 3,00', 'Switchif™'),
+(7, 'Pão Recheado', 'Calabresa', 'R$ 5,00', 'Switchif™'),
+(8, 'Pão Recheado Doce', 'Doce', 'R$ 4,00', 'Switchif™'),
+(42, 'Pão da Vida', 'A Verdade', 'R$ 500,00', 'Switchif™');
 
 ALTER TABLE `Paes`
   ADD PRIMARY KEY (`id`);
